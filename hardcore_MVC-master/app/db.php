07d2429e47444587cac9f151dc6fafb0ac8be639
Unknown <max.garnier@live.fr>
2018-04-04 16:16:29 +0200

    modification base MVC

--- hardcore_MVC-master/app/db.php
+++ hardcore_MVC-master/app/db.php
@@ -1,6 +1,6 @@
 <?php
 
 define("APP_DB_HOST","localhost");
-define("APP_DB_NAME","MVC");
-define("APP_DB_USER","eleve");
-define("APP_DB_PWD", "eleve");
+define("APP_DB_NAME","Musicband");
+define("APP_DB_USER","Maxime");
+define("APP_DB_PWD", "Panoplie80");
