dc8362105cb93b282d37583a62c1d4d938ee0a94
Unknown <max.garnier@live.fr>
2018-04-21 00:34:57 +0200

    ajout vérification mdp page admin

--- hardcore_MVC-master/src/Controller/AdminController.php
+++ hardcore_MVC-master/src/Controller/AdminController.php
@@ -23,13 +23,18 @@ class AdminController extends AbstractController
     public function adminPage()
     {
         session_start();
-        if (($_SERVER['REQUEST_METHOD']==='POST') && (isset($_POST['user'])) && (isset($_POST['pwd']))){
-            $_SESSION['user'] = $_POST['user'];
-            $_SESSION['pwd'] = $_POST['pwd'];
-            return $this->twig->render('Admin/adminPage.html.twig');
-        }
+        $vipUser="Franck-O";
+        $vipPwd="Wilder4life";
+        if (($_SERVER['REQUEST_METHOD']==='POST') && (isset($_POST['user'])) && (isset($_POST['pwd']))) {
 
-       // header('/login');
+            if ((isset($_POST["user"])) && (($_POST["user"]) == $vipUser) && (isset($_POST["pwd"])) && (($_POST["pwd"]) == $vipPwd)) {
+                $_SESSION['user'] = $_POST['user'];
+                $_SESSION['pwd'] = $_POST['pwd'];
+                return $this->twig->render('Admin/adminPage.html.twig');
+            } else
+                $Error = "Veuillez entrer un couple nom d'utilisateur et mot de passe valide";
+                return $this->twig->render('Admin/index.html.twig');
+        }
     }
 
 
